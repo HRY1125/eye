6b6e5fa6603a67fecc4ec0913b87869da8a8a15d
HRY <2098506133@qq.com>
2020-04-27 09:58:05 +0800

    first commit

---
+++ src/main/java/com/zlk/eye/EyeApplication.java
@@ -0,0 +1,13 @@
+package com.zlk.eye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyeApplication.class, args);
+    }
+
+}
